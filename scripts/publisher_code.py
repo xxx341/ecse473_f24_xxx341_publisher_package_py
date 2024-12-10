e1d918076d3927fd5518f1a00f4ac0240b4a04b7
Xin <xxx341@case.edu>
2024-12-10 08:32:44 -0500

    final lab 1

--- scripts/publisher_code.py
+++ scripts/publisher_code.py
@@ -4,7 +4,7 @@ import rospy
 from std_msgs.msg import String
 
 def talker():
-    pub = rospy.Publisher('publisher_topic', String, queue_size=10)
+    pub = rospy.Publisher('chatter', String, queue_size=10)
     rospy.init_node('publisher_node', anonymous=True)
     rate = rospy.Rate(1) # 1 Hz
     while not rospy.is_shutdown():
